6a1bf825da2f2ca3f0b4e64f30720b0055a097ad
codeigniter-test-emails <siromunya@gmail.com>
2022-07-04 13:22:58 +0300

    codeigniter test emails PoC code files

---
+++ application/config/email_trans.php
@@ -0,0 +1,39 @@
+<?php 
+
+$config['smtp_crypto'] = 'tls'; 
+
+$config['useragent'] = 'CodeIgniter';
+
+$config['protocol'] = 'smtp';
+
+$config['smtp_host'] = 'smtp.office365.com';
+
+$config['smtp_user'] = 'intranet@transcentury.co.ke';
+
+$config['smtp_pass'] = 'Nairobi@2024'; 
+
+$config['smtp_port'] = 587; //465 //587
+
+$config['smtp_timeout'] = 5;
+
+$config['wordwrap'] = TRUE;
+
+$config['wrapchars'] = 76;
+
+$config['mailtype'] = 'html';
+
+$config['charset'] = 'utf-8';
+
+$config['validate'] = FALSE;
+
+$config['priority'] = 3;
+
+$config['crlf'] = "\r\n";
+
+$config['newline'] = "\r\n";
+
+$config['bcc_batch_mode'] = FALSE;
+
+$config['bcc_batch_size'] = 200;
+ 
+?>
